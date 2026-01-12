89f7323976f760be85e293ab0e47a4e873e4ec77
youssefyouyoudev <contact@youssefyouyou.com>
2026-01-12 12:09:23 +0100

    hh555

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->uuid('uuid')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'agent', 'client'])->default('client');
             $table->rememberToken();
-            $table->enum('role', ['admin', 'agent'])->default('agent');
             $table->timestamps();
             $table->softDeletes();
         });
