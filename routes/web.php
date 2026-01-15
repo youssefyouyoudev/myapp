c08fb5e7d33214c3ccf30eb93727dd635c032126
youssefyouyoudev <contact@youssefyouyou.com>
2026-01-15 09:48:16 +0100

    555

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ Route::get('/game', function() { return view('game.home'); })->name('game.home')
 Route::get('/game/categories', function() { return view('game.categories'); })->name('game.categories');
 Route::get('/game/category', function() { return view('game.game'); })->name('game.category');
 Route::get('/game/result', function() { return view('game.result'); })->name('game.result');
+Route::get('/plan', function() { return view('game.plan'); })->name('game.plan');
