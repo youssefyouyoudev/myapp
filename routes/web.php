61275ffd15f79789af2184ba2abc2f07191d4d4f
youssefyouyoudev <contact@youssefyouyou.com>
2026-01-16 12:26:48 +0100

    555

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::post('/contact', [HomeController::class, 'contact'])->name('contact');
 Route::view('/api-tester', 'test.testclientpost');
 
-// Marriage Game routes
-Route::get('/game', function() { return view('game.home'); })->name('game.home');
-Route::get('/game/categories', function() { return view('game.categories'); })->name('game.categories');
-Route::get('/game/category', function() { return view('game.game'); })->name('game.category');
-Route::get('/game/result', function() { return view('game.result'); })->name('game.result');
-Route::get('/plan', function() { return view('game.plan'); })->name('game.plan');
+// // Marriage Game routes
+// Route::get('/game', function() { return view('game.home'); })->name('game.home');
+// Route::get('/game/categories', function() { return view('game.categories'); })->name('game.categories');
+// Route::get('/game/category', function() { return view('game.game'); })->name('game.category');
+// Route::get('/game/result', function() { return view('game.result'); })->name('game.result');
+// Route::get('/plan', function() { return view('game.plan'); })->name('game.plan');
