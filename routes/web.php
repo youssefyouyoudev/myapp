773bf752a97f3ccd88249d300576fb215fcdc963
youssefyouyoudev <contact@youssefyouyou.com>
2026-01-21 02:24:19 +0100

    ddd465

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::view('/api-tester', 'test.api-tester')->name('api-tester');
 Route::view('/planstest', 'test.planstest')->name('planstest');
 
 // // Marriage Game routes
-// Route::get('/game', function() { return view('game.home'); })->name('game.home');
+Route::get('/game', function() { return view('game.home'); })->name('game.home');
 // Route::get('/game/categories', function() { return view('game.categories'); })->name('game.categories');
 // Route::get('/game/category', function() { return view('game.game'); })->name('game.category');
 // Route::get('/game/result', function() { return view('game.result'); })->name('game.result');
