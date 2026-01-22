06d1ad5f04babde48bc522ad47dd75f5b7d005c7
youssefyouyoudev <contact@youssefyouyou.com>
2026-01-22 13:11:34 +0100

    ddd46dddd5

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Log extends Model
 {
-        protected $fillable = [
-            'user_id',
-            'action',
-            'model',
-            'model_id',
-            'details',
-        ];
+
+    protected $fillable = [
+        'user_id',
+        'action',
+        'model',
+        'model_id',
+        'details',
+    ];
+}
