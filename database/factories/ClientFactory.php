067f7c48ddbeccc2d6b856cd699125e9f4574152
youssefyouyoudev <contact@youssefyouyou.com>
2026-01-22 11:06:14 +0100

    ddd465

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -16,7 +16,7 @@ class ClientFactory extends Factory
             'user_id' => null, // Set in seeder if needed
             'full_name' => $this->faker->name(),
             'phone' => $this->faker->phoneNumber(),
-            'status' => $this->faker->randomElement(['active', 'inactive']),
+            'status' => "active",
             'cin' => strtoupper(Str::random(8)),
             'date_of_birth' => $this->faker->date('Y-m-d', '-18 years'),
             'school' => $this->faker->company(),
