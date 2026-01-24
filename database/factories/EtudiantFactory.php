cf5db27da15f9a11a6d3d2a7f4528c2a2105cea3
youssefyouyoudev <contact@youssefyouyou.com>
2026-01-24 13:06:41 +0100

    ddd46dddd5

--- database/factories/EtudiantFactory.php
+++ database/factories/EtudiantFactory.php
@@ -6,21 +6,26 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Client>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Etudiant>
  */
 class EtudiantFactory extends Factory
 {
-    public function definition(): array
-    {
-        return [
-          'user_id' => \App\Models\User::factory(),
-          'full_name' => $this->faker->name(),
-          'phone' => $this->faker->phoneNumber(),
-          'status' => $this->faker->randomElement(['active', 'suspended']),
-          'cin' => $this->faker->unique()->bothify('??######'),
-          'date_of_birth' => $this->faker->date(),
-          'school' => $this->faker->company(),
-          'user_id' => 2,
-        ];
-    }
+  public function definition(): array
+  {
+    return [
+      'user_id' => \App\Models\User::factory(),
+      'nom' => $this->faker->lastName(),
+      'prenom' => $this->faker->firstName(),
+      'etablissement' => $this->faker->company(),
+      'email' => $this->faker->unique()->safeEmail(),
+      'telephone' => $this->faker->phoneNumber(),
+      'adresse' => $this->faker->address(),
+      'carte_nationale' => $this->faker->unique()->bothify('??######'),
+      'carte_etudiant' => $this->faker->optional()->bothify('ETU######'),
+      'img_user' => null,
+      'img_carte_nationale' => null,
+      'img_carte_nationale_verso' => null,
+      'img_carte_etudiant' => null,
+    ];
+  }
 }
