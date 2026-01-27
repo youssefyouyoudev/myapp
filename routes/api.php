db2e68010089eff813ba24c186c5bc1425490644
youssefyouyoudev <contact@youssefyouyou.com>
2026-01-27 13:30:19 +0100

    ddd46dddd5

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->group(function () {
 				// Card validation (subscription/voyage logic)
 				Route::post('cards/{card}/validate', [CardController::class, 'validateCard']);
 			Route::get('logs', [\App\Http\Controllers\LogController::class, 'index']);
-			Route::get('logs/user/{user_id}', [\App\Http\Controllers\LogController::class, 'getByUserId']);
+			Route::get('logs/user/{user_id}', [\App\Http\Controllers\LogController::class, 'getByUserIdAndDateRange']);
 		// Logs
 		Route::post('logs/bulk', [\App\Http\Controllers\LogController::class, 'store']);
 	// Auth
