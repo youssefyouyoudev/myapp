b2ceed3fd4b9a0306d5754e2f569301cb0e15dd3
youssefyouyoudev <contact@youssefyouyou.com>
2026-01-27 13:52:46 +0100

    ddd46dddd5

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\EtudiantController;
 Route::post('login', [AuthController::class, 'login']);
 
 // Protected routes
-// Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
 				// Card validation (subscription/voyage logic)
 				Route::post('cards/{card}/validate', [CardController::class, 'validateCard']);
 			Route::get('logs', [\App\Http\Controllers\LogController::class, 'index']);
@@ -73,4 +73,4 @@ Route::post('login', [AuthController::class, 'login']);
 
 	// Dashboard route
 	Route::get('dashboard', [DashboardController::class, 'index']);
-// });
+});
